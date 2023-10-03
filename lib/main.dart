b85f7951731109c2bc9aeae32e8e42611ae7e840
basel embaby <engbasel2025@gmail.com>
2023-10-03 22:07:09 +0300

    change theme and create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notesappv2/Screens/HomeScreen.dart';
+import 'package:notesappv2/viwes/Notes_viwe.dart';
 
 void main(List<String> args) {
   runApp(const notesAppv2());
@@ -10,10 +10,11 @@ class notesAppv2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      routes: {},
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const Notes_viwe(),
+      routes: const {},
     );
   }
 }
---
+++ lib/viwes/Notes_viwe.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Notes_viwe extends StatelessWidget {
+  const Notes_viwe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
