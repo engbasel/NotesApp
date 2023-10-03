b87297ebbcfc73dff269bf9190b8a6839ddada96
basel embaby <engbasel2025@gmail.com>
2023-10-03 23:54:53 +0300

    add note model

---
+++ lib/Models/noteModel.dart
@@ -0,0 +1,12 @@
+class NoteMode1 {
+  final String title;
+  final String subTit1e;
+  final String data;
+  final int color;
+  NoteMode1({
+    required this.title,
+    required this.subTit1e,
+    required this.data,
+    required this.color,
+  });
+}
